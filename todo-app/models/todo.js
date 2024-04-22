3e8c6bdf9205291782e6cf2489bcb11701a94651
Praveen-1771 <141398895+Praveen-1771@users.noreply.github.com>
2024-04-22 12:16:05 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
    
     static associate(models) {
       // define association here
-      Todo.belongsTo(models.User, { });
+    
     }
 
     static addTodo({ title, dueDate }) {
