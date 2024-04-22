22924b92596db87868b1efc2bf02ec8c63999ce7
Praveen-1771 <141398895+Praveen-1771@users.noreply.github.com>
2024-04-22 12:38:28 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -10,7 +10,7 @@ app.set('view engine','ejs');
 
 
 app.get("/", async (request, response) => {
-  const allTodos=await Todo.getTodos();
+  const allTodos = await Todo.getTodos(); 
   if(request.accepts('html')) {
     response.render('index',{
     allTodos
