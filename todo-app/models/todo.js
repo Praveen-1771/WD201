61baa36cb0b41b2f06a64d6340cb613127284d87
Praveen-1771 <141398895+Praveen-1771@users.noreply.github.com>
2024-04-22 12:03:53 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -7,9 +7,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
-      Todo.belongsTo(models.User, { foreignKey: 'userId' }); // Assuming a User model with a primary key 'id'
+   
     
     }
 
